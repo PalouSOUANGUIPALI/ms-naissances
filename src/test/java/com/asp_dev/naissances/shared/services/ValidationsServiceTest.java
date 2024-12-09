ce38d4ffe0c46ae7a4b04874fbb4d60a5d323de4
PalouSOUANGUIPALI <samparialzouma91@gmail.com>
2024-12-09 14:11:26 +0100

    initial commit

---
+++ src/test/java/com/asp_dev/naissances/shared/services/ValidationsServiceTest.java
@@ -0,0 +1,16 @@
+package com.asp_dev.naissances.shared.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationsServiceTest {
+
+    @Test
+    void validateEmail() {
+    }
+
+    @Test
+    void validatePhoneNumber() {
+    }
+}
