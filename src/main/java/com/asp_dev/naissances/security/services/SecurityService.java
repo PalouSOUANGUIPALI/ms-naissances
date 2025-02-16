509c856ebdefa6b7d5b720885df09ed168889e9d
PalouSOUANGUIPALI <samparialzouma91@gmail.com>
2025-02-16 17:46:35 +0100

    minor updating securityservice

--- src/main/java/com/asp_dev/naissances/security/services/SecurityService.java
+++ src/main/java/com/asp_dev/naissances/security/services/SecurityService.java
@@ -1,18 +1,28 @@
 package com.asp_dev.naissances.security.services;
 
 import com.asp_dev.naissances.profiles.entities.Profiles;
+import com.asp_dev.naissances.profiles.repository.ProfilesRepository;
+
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@AllArgsConstructor
 @Component
 public class SecurityService {
+    private final ProfilesRepository profilesRepository;
+
 
     public Profiles getCurrentUser(){
-        return
-                (Profiles) SecurityContextHolder
+        Jwt jwt = (Jwt) SecurityContextHolder
                         .getContext()
                         .getAuthentication()
                         .getPrincipal();
-
+        String email = jwt.getSubject();
+        Optional<Profiles> profiles = profilesRepository.findByEmail(email);
+        return profiles.orElseThrow(() -> new RuntimeException("Aucune response ne correspond aux paramètres fournis"));
     }
 }
